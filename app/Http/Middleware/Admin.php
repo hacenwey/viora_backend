d5bacb72d1320f98ff1977ad3615a11dfe7c4dac
Administrator <admin@example.com>
2022-04-10 18:15:16 +0000

    Revert "Update Admin.php"
    This reverts commit c5137c154e77a20c96506e82294c22bfebdec35c

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -1,1 +1,27 @@
-aklhfklahfjk ajkafh ka
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class Admin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(auth()->user()->is_admin){
+            return $next($request);
+        }
+        else{
+            request()->session()->flash('error','You do not have any permission to access this page');
+            return redirect()->route('backend.home');
+        }
+    }
+}
