ca5808718aff5fab0de0b48fe2052ecedae7f9da
El Hacen Wey <hacen.wey@dev.awlyg.tech>
2022-06-22 22:07:58 +0000

    fix bug

--- resources/views/backend/commandes/index.blade.php
+++ resources/views/backend/commandes/index.blade.php
@@ -43,7 +43,7 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h6 class="modal-title" id="provider_modalLabel">Ajouter Fourniseur</h6>
+                    <h6 class="modal-title" id="provider_modalLabel">Ajouter une commande</h6>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
