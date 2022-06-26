3c1dd709eb40f0fab66d528f16398900c6999649
Mohamed Yehdhih <elmoctar.yehdhih@gmail.com>
2022-06-27 00:38:50 +0100

    fixe rbug

--- resources/views/backend/currencys/index.blade.php
+++ resources/views/backend/currencys/index.blade.php
@@ -1,158 +1,164 @@
 @extends('backend.layouts.master')
-@section('title',settings()->get('app_name').' | '. 'Supplier management')
+@section('title', settings()->get('app_name') . ' | ' . 'Supplier management')
 @section('main-content')
- <!-- DataTales Example -->
- <div class="card shadow mb-4">
-     <div class="row">
-         <div class="col-md-12">
-            @include('backend.layouts.notification')
-         </div>
-     </div>
-    <div class="card-header py-3">
-      <h6 class="m-0 font-weight-bold text-primary float-left">Currencys @lang('global.list')</h6>
-      <a href="" data-toggle="modal" data-target="#exampleModal" class="btn btn-primary btn-sm float-right" data-toggle="tooltip" data-placement="bottom" title="@lang('global.new') @lang('cruds.brand.title_singular')"><i class="fas fa-plus"></i>Ajouter Devise</a>
+    <!-- DataTales Example -->
+    <div class="card shadow mb-4">
+        <div class="row">
+            <div class="col-md-12">
+                @include('backend.layouts.notification')
+            </div>
+        </div>
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary float-left">Currencys @lang('global.list')</h6>
+            <a href="" data-toggle="modal" data-target="#exampleModal" class="btn btn-primary btn-sm float-right"
+                data-toggle="tooltip" data-placement="bottom" title="@lang('global.new') @lang('cruds.brand.title_singular')"><i
+                    class="fas fa-plus"></i>Ajouter Devise</a>
 
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" id="brand-dataTable" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th>Code</th>
+                            <th>Name</th>
+                            <td>Taux de change</td>
+                            <th>@lang('global.action')</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($currencys as $currency)
+                            <tr>
+                                <td>{{ $currency->code }}</td>
+                                <td>{{ $currency->name }}</td>
+                                <td>{{ $currency->exchange_rate }}</td>
+
+                                <td>
+                                    <a href="{{ route('backend.provider.edit', $currency->id) }}"
+                                        class="btn btn-primary btn-sm float-left mr-1"
+                                        style="height:30px; width:30px;border-radius:50%" data-toggle="tooltip"
+                                        title="@lang('global.edit')" data-placement="bottom"><i class="fas fa-edit"></i></a>
+                                    <form method="POST" action="{{ route('backend.provider.destroy', [$currency->id]) }}">
+                                        @csrf
+                                        @method('delete')
+                                        <button class="btn btn-danger btn-sm dltBtn" data-id={{ $currency->id }}
+                                            style="height:30px; width:30px;border-radius:50%" data-toggle="tooltip"
+                                            data-placement="bottom" title="@lang('global.delete')"><i
+                                                class="fas fa-trash-alt"></i></button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+                <span style="float:right">{{ $currencys->links() }}</span>
+            </div>
+        </div>
     </div>
-    <div class="card-body">
-      <div class="table-responsive">
-        <table class="table table-bordered" id="brand-dataTable" width="100%" cellspacing="0">
-          <thead>
-            <tr>
-              <th>Code</th>
-              <th>Name</th>
-              <th>Status</th>
-              <td>Toux change</td>
-              <th>@lang('global.action')</th>
-            </tr>
-          </thead>
-          <tbody>
-            @foreach($currencys as $currency)
-            <tr>
-                <td>{{$currency->code}}</td>
-                <td>{{$currency->name}}</td>
-                <td>{{$currency->status}}</td>
-                <td>{{$currency->exchange_rate}}</td>
 
-                <td>
-                    <a href="{{route('backend.provider.edit',$currency->id)}}" class="btn btn-primary btn-sm float-left mr-1" style="height:30px; width:30px;border-radius:50%" data-toggle="tooltip" title="@lang('global.edit')" data-placement="bottom"><i class="fas fa-edit"></i></a>
-                    <form method="POST" action="{{route('backend.provider.destroy',[$currency->id])}}">
-                      @csrf
-                      @method('delete')
-                          <button class="btn btn-danger btn-sm dltBtn" data-id={{$currency->id}} style="height:30px; width:30px;border-radius:50%" data-toggle="tooltip" data-placement="bottom" title="@lang('global.delete')"><i class="fas fa-trash-alt"></i></button>
-                    </form>
-                </td>
-            </tr>
-        @endforeach
-          </tbody>
-        </table>
-        <span style="float:right">{{$currencys->links()}}</span>
-      </div>
+
+    <!-- Modal Add Suppliers-->
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Add Suppliers</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <form method="post" action="{{ route('backend.currencys.store') }}">
+                    {{ csrf_field() }}
+                    <div class="modal-body">
+                        <label for="exampleInputEmail1">Name</label>
+                        <input type="text" name="name" class="form-control" id="exampleInputEmail1"
+                            aria-describedby="emailHelp">
+                        <label for="exampleInputEmail1">Code</label>
+                        <input type="text" name="code" class="form-control" id="exampleInputEmail1"
+                            aria-describedby="emailHelp">
+                        <label for="exampleInputEmail1">Taux change </label>
+                        <input type="text" name="exchange_rate" class="form-control" id="exampleInputEmail1"
+                            aria-describedby="emailHelp">
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Save changes</button>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
-</div>
-
-
-<!-- Modal Add Suppliers-->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Add Suppliers</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <form method="post" action="{{route('backend.currencys.store')}}">
-        {{csrf_field()}}
-      <div class="modal-body">
-        <label for="exampleInputEmail1">Name</label>
-    <input type="text" name="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" >
-    <label for="exampleInputEmail1">Code</label>
-    <input type="text" name="code" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" >
-    <label for="exampleInputEmail1">Taux change </label>
-    <input type="text" name="exchange_rate" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" >
-    <label for="exampleInputEmail1">Status</label>
-    <input type="text" class="form-control" name="status"   id="exampleInputEmail1" aria-describedby="emailHelp" >
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="submit" class="btn btn-primary">Save changes</button>
-      </div>
-    </form>
-    </div>
-  </div>
-</div>
 @endsection
 
 @push('styles')
-  <link href="{{asset('backend/vendor/datatables/dataTables.bootstrap4.min.css')}}" rel="stylesheet">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.css" />
-  <style>
-      div.dataTables_wrapper div.dataTables_paginate{
-          display: none;
-      }
-      .zoom {
-        transition: transform .2s; /* Animation */
-      }
+    <link href="{{ asset('backend/vendor/datatables/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.css" />
+    <style>
+        div.dataTables_wrapper div.dataTables_paginate {
+            display: none;
+        }
 
-      .zoom:hover {
-        transform: scale(3.2);
-      }
-  </style>
+        .zoom {
+            transition: transform .2s;
+            /* Animation */
+        }
+
+        .zoom:hover {
+            transform: scale(3.2);
+        }
+    </style>
 @endpush
 
 @push('scripts')
+    <!-- Page level plugins -->
+    <script src="{{ asset('backend/vendor/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('backend/vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
 
-  <!-- Page level plugins -->
-  <script src="{{asset('backend/vendor/datatables/jquery.dataTables.min.js')}}"></script>
-  <script src="{{asset('backend/vendor/datatables/dataTables.bootstrap4.min.js')}}"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
-
-  <!-- Page level custom scripts -->
-  <script src="{{asset('backend/js/demo/datatables-demo.js')}}"></script>
-  <script>
-
-      $('#brand-dataTable').DataTable( {
-            "columnDefs":[
-                {
-                    "orderable":false,
-                    "targets":[3,4]
-                }
-            ]
-        } );
+    <!-- Page level custom scripts -->
+    <script src="{{ asset('backend/js/demo/datatables-demo.js') }}"></script>
+    <script>
+        $('#brand-dataTable').DataTable({
+            "columnDefs": [{
+                "orderable": false,
+                "targets": [3, 4]
+            }]
+        });
 
         // Sweet alert
 
-        function deleteData(id){
+        function deleteData(id) {
 
         }
-  </script>
-  <script>
-      $(document).ready(function(){
-        $.ajaxSetup({
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            }
-        });
-          $('.dltBtn').click(function(e){
-            var form=$(this).closest('form');
-              var dataID=$(this).data('id');
-              // alert(dataID);
-              e.preventDefault();
-              swal({
-                    title: "{!! trans('global.areYouSure') !!}",
-                    text: "{!! trans('global.delete_warning') !!}",
-                    icon: "warning",
-                    buttons: true,
-                    dangerMode: true,
-                })
-                .then((willDelete) => {
-                    if (willDelete) {
-                       form.submit();
-                    } else {
-                        swal("{!! trans('global.data_is_safe') !!}");
-                    }
-                });
-          })
-      })
-  </script>
+    </script>
+    <script>
+        $(document).ready(function() {
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+            $('.dltBtn').click(function(e) {
+                var form = $(this).closest('form');
+                var dataID = $(this).data('id');
+                // alert(dataID);
+                e.preventDefault();
+                swal({
+                        title: "{!! trans('global.areYouSure') !!}",
+                        text: "{!! trans('global.delete_warning') !!}",
+                        icon: "warning",
+                        buttons: true,
+                        dangerMode: true,
+                    })
+                    .then((willDelete) => {
+                        if (willDelete) {
+                            form.submit();
+                        } else {
+                            swal("{!! trans('global.data_is_safe') !!}");
+                        }
+                    });
+            })
+        })
+    </script>
 @endpush
