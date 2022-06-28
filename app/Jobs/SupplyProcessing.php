fb8f39f52bfc741ef24e1c2eb05911ee24f12014
Mohamed Yehdhih <elmoctar.yehdhih@gmail.com>
2022-06-28 16:32:48 +0100

    Foixer bug

--- app/Jobs/SupplyProcessing.php
+++ app/Jobs/SupplyProcessing.php
@@ -45,7 +45,7 @@ class SupplyProcessing implements ShouldQueue
             $journal = [];
             $open = fopen(storage_path('app/public/cdn/files/' . $this->import['file_name']), "r");
             if ($open !== FALSE) {
-                $delimiter = $this->detectDelimiter($open);
+                $delimiter = $this->detectDelimiter();
                 while (($data = fgetcsv($open, 1000, $delimiter)) !== FALSE) {
                     $journal[] = $data;
                 }
@@ -117,8 +117,9 @@ class SupplyProcessing implements ShouldQueue
      * @param string $csvFile Path to the CSV file
      * @return string Delimiter
      */
-    public function detectDelimiter($handle)
+    public function detectDelimiter()
     {
+        $handle = fopen(storage_path('app/public/cdn/files/' . $this->import['file_name']), "r");
         $delimiters = [";" => 0, "," => 0, "\t" => 0, "|" => 0];
 
         $firstLine = fgets($handle);
