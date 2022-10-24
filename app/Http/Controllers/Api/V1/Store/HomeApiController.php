081fa0bc9ad87360126f982b2428ad059d4da295
hacen wey <elhassenidoumo@gmail.com>
2022-10-24 17:14:28 +0000

    add rendom order by to listing product

--- app/Http/Controllers/Api/V1/Store/HomeApiController.php
+++ app/Http/Controllers/Api/V1/Store/HomeApiController.php
@@ -35,7 +35,7 @@ class HomeApiController extends Controller
     public function index(Request $request)
     {
         if ($request->section == 'categories') {
-            $categories = Category::where('status', 'active')->oorderBy(DB::raw('RAND()'))->get();
+            $categories = Category::where('status', 'active')->orderBy(DB::raw('RAND()'))->get();
             return response()->json([
                 'title' => 'Categories',
                 'enabled' => true,
