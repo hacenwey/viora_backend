62a4e4661d252039c89e897de9a622cb2e2340e8
cheikh.elkhou <cheikhmed2019a@gmail.com>
2022-10-27 13:09:23 +0100

    modification

--- database/seeders/ProvincesTableSeeder.php
+++ database/seeders/ProvincesTableSeeder.php
@@ -19,59 +19,99 @@ class ProvincesTableSeeder extends Seeder
 
                 [
                     'name' => 'Nouadhibou',
-                    'state_id' => '2'
+                    'state_id' => '4'
 
                 ],
                 [
                     'name' => 'Chammi',
-                    'state_id' => '2'
+                    'state_id' => '4'
 
                 ],
                 [
                     'name' => 'Dar naim',
-                    'state_id' => '13'
+                    'state_id' => '1'
 
                 ],
                 [
                     'name' => 'Tayar ',
-                    'state_id' => '13'
+                    'state_id' => '1'
 
                 ],
                 [
                     'name' => 'Toujounine',
-                    'state_id' => '13'
+                    'state_id' => '1'
 
                 ],
                 [
                     'name' => 'Tevragh zeina ',
-                    'state_id' => '14'
+                    'state_id' => '2'
 
                 ],
                 [
                     'name' => 'Lksar',
-                    'state_id' => '14'
+                    'state_id' => '2'
 
                 ],
                 [
                     'name' => 'Sabkha',
-                    'state_id' => '14'
+                    'state_id' => '2'
 
                 ],
 
 
                 [
                     'name' => 'Arafat',
-                    'state_id' => '15'
+                    'state_id' => '3'
 
                 ],
                 [
                     'name' => 'EL Mina',
-                    'state_id' => '15'
+                    'state_id' => '3'
 
                 ],
                 [
                     'name' => 'Riyad',
-                    'state_id' => '15'
+                    'state_id' => '3'
+
+                ],
+                [
+                    'name' => 'Aleg',
+                    'state_id' => '8'
+
+                ],
+                [
+                    'name' => 'Bababe',
+                    'state_id' => '8'
+
+                ],
+                [
+                    'name' => 'Magta-Lahjar',
+                    'state_id' => '8'
+
+                ],
+                [
+                    'name' => 'Bogué',
+                    'state_id' => '8'
+
+                ],
+                [
+                    'name' => 'Ayoun al_Atrous',
+                    'state_id' => '5'
+
+                ],
+                [
+                    'name' => 'Tintane',
+                    'state_id' => '5'
+
+                ],
+                [
+                    'name' => 'Kobeni',
+                    'state_id' => '5'
+
+                ],
+                [
+                    'name' => 'Tamchakett',
+                    'state_id' => '5'
 
                 ],
 
