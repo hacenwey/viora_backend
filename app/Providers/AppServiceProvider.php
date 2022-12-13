176b7f8c4af473efc9446218e54810cf084f7914
hacen wey <elhassenidoumo@gmail.com>
2022-12-13 11:09:43 +0000

    fix bugs 419

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,6 +29,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $this->app['request']->server->set('HTTPS','on');
+
         if($this->app->environment('production')) {
             \URL::forceScheme('https');
         }
