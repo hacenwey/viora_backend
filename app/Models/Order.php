d9cefc2d21551ceaec6e4ee8e4f92f884a9568aa
Mohamed Yehdhih <elmoctar.yehdhih@gmail.com>
2023-05-15 10:42:03 +0000

    Update Order.php

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -37,11 +37,7 @@ class Order extends Model implements Searchable
     {
         parent::boot();
         self::creating(function ($model) {
-            $random_number = rand(1000, 9999);
-            $next_reference = str_pad(Order::all()->max('reference') + 1, 6, 0, STR_PAD_LEFT);
-            $reference = 'OR-' . $random_number . '-' . $next_reference;
-            
-            $model->reference = $reference;        
+            $model->reference = 'OR-' . strtoupper(uniqid()); 
         });
     }
 
