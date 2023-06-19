65e738571b51f73d9b7ba8d6e755d8f7b62b154d
hacen wey <elhassenidoumo@gmail.com>
2023-06-19 17:02:26 +0000

    fix popular product

--- app/Http/Controllers/Api/V1/Store/HomeApiController.php
+++ app/Http/Controllers/Api/V1/Store/HomeApiController.php
@@ -79,9 +79,9 @@ class HomeApiController extends Controller
                 $popular = Cache::remember('popular', self::EXPIRATION_TIME, function () {
                     return Product::with('categories')->where('status', 'active')->where('stock', '!=', 0)
                         ->leftJoin('order_products', 'products.id', '=', 'order_products.product_id')
-                        ->selectRaw('products.*, COALESCE(sum(order_products.quantity),0) total')
+                        ->selectRaw('products.*, COALESCE(sum(order_products.product_id),0) total')
                         ->groupBy('products.id')
-                        ->orderBy('id', 'ASC')
+                        ->orderBy('total', 'DESC')
                         ->get();
                 });
                 if ($request->limit > 0) {
@@ -108,7 +108,7 @@ class HomeApiController extends Controller
 
             case 'return_in_stock':
                 $return_in_stock = Cache::remember('return_in_stock', self::EXPIRATION_TIME, function () {
-                    return Product::where('status', 'active')->where('stock', '!=', 0)->where('stock_last_update', '>', Carbon::now()->subDays(21))->orderBy('id', 'ASC')->limit(9)->get();
+                    return Product::where('status', 'active')->where('stock', '!=', 0)->where('stock_last_update', '>', Carbon::now()->subDays(21))->orderBy('id', 'ASC')->get();
                 });
                 if ($request->limit > 0) {
                     $return_in_stock = $return_in_stock->take($request->limit);
