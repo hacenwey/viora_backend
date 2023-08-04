23d8d3b7707e985a294f7319b3c1a8646bfd5a28
hacen wey <elhassenidoumo@gmail.com>
2023-08-04 11:14:55 +0000

    add user type to nullable

--- database/migrations/2023_08_03_173537_add_type_to_users_table.php
+++ database/migrations/2023_08_03_173537_add_type_to_users_table.php
@@ -14,7 +14,7 @@ class AddTypeToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->enum('type',['CUSTOMER','SELLER'])->default('CUSTOMER');
+            $table->enum('type',['CUSTOMER','SELLER'])->nullable();
             $table->string('age')->nullable();
             $table->string('occupation')->nullable();
 
